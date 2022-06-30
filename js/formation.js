70fa3cdff4b29f8b16a1d5591f374950aaa31ee6
blakpsykozz27 <attitude27@yahoo.fr>
2022-06-30 11:15:26 +0200

    camarchepas

---
+++ js/formation.js
@@ -0,0 +1,11 @@
+
+//let array= ['git', 'mod1', 'mod4', 'mod5', 'scrum']
+//let result = array.filter(function(element) {
+ //   return element == 'mod4';
+ // });
+
+ // console.log(result);
+ // console.log(array);
+
+
+
