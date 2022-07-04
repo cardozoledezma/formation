4cd672a53b1251c9bba7e39d857a968e690df0a7
Julien Richard <julien.richard.lh@gmail.com>
2022-07-04 09:50:08 +0200

    search bar

--- js/script.js
+++ js/script.js
@@ -31,17 +31,19 @@ document.getElementById("everything").addEventListener("click",function (event){
 
 //Search-bar keyword filter
 
-function searchSubject() {
-    let input = document.getElementById('search-bar').value
-    input=input.toLowerCase();
-    let x = document.getElementsByClassName('course');
-      
-    for (i = 0; i < x.length; i++) { 
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display="none";
-        }
-        else {
-            x[i].style.display="course-list";                 
+function searchBar() {
+    var input, filter, ul, li, a, txtValue;
+    input = document.getElementById("search-bar");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("course-list");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
         }
     }
 }
